2ca9b8305c38e2b940a9b2f3277392ed1eeea679
hemantvsn <hemantvsn@gmail.com>
2015-04-18 13:29:52 +0530

    Added BONECP for database connection pooling.

--- src/main/java/com/shasm/util/DBUtil.java
+++ src/main/java/com/shasm/util/DBUtil.java
@@ -5,31 +5,88 @@ import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.EntityTransaction;
 
-import com.sun.security.auth.module.UnixSystem;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import com.jolbox.bonecp.BoneCPDataSource;
+import com.shasm.model.users.User;
+
 
 public class DBUtil {
+
+	private static final Map<Integer, EntityManagerFactory> entityManagerFactories = new HashMap<Integer, EntityManagerFactory>();
+
+	
+	
+	/**
+	 * Need Threadsafe implementation
+	 * This EntityManager, Datasource, JPA vendor config is sufficient
+	 * We no longer need persistence.xml
+	 * @param orgId
+	 * @return
+	 */
 	public synchronized static EntityManager initEntityManager(Integer orgId) {
+		
+		// fallback Approach
 		if (orgId == null) {
-			orgId = 0;
+			orgId = 1;
 		}
 
-		UnixSystem system = new UnixSystem();
-		EntityManagerFactory entityManagerFactory = null;
-		Map<String, String> properties = new HashMap<String, String>();
-		properties.put("javax.persistence.jdbc.url",
-				"jdbc:postgresql://127.0.0.1/testqube"
-						+ ((orgId > 0) ? orgId : ""));
-		properties.put("javax.persistence.jdbc.user", system.getUsername());
-		properties.put("javax.persistence.jdbc.password", "");
+		EntityManagerFactory factory = entityManagerFactories.get(orgId);
 
-		entityManagerFactory = Persistence.createEntityManagerFactory("SHASM",
-				properties);
+		if (factory == null) {
+			LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		    factoryBean.setPersistenceProvider(new HibernatePersistenceProvider());
+		    factoryBean.setPackagesToScan("com.shasm.model");
+		    factoryBean.setDataSource(createDataSource(orgId));
+		    factoryBean.setJpaVendorAdapter(getJPAVendorAdapter());
+		    factoryBean.afterPropertiesSet();
+		    factory = factoryBean.getNativeEntityManagerFactory();
+		    entityManagerFactories.put(orgId, factory);
+		}
+		return factory.createEntityManager();
 
-		EntityManager entityManager = entityManagerFactory
-				.createEntityManager();
+	}
 
-		return entityManager;
+	private static BoneCPDataSource createDataSource(int orgId) {
+		BoneCPDataSource ds = new BoneCPDataSource();
+		ds.setJdbcUrl("jdbc:postgresql://127.0.0.1/testqube" + orgId);
+		ds.setUser("hemantvsn");
+		ds.setPassword("");
+		ds.setDriverClass("org.postgresql.Driver");
+		ds.setPartitionCount(3);
+		ds.setMinConnectionsPerPartition(5);
+		ds.setMaxConnectionsPerPartition(15);
+		return ds;
+
+	}
+	
+	private static HibernateJpaVendorAdapter getJPAVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.POSTGRESQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+		
+		return adapter;
+	}
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+		EntityManager em = initEntityManager(1);
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		
+		User user = new User();
+		user.setEmail("xxx");
+		em.persist(user);
+		transaction.commit();
+		}
+			
+		System.out.println("---------------------------------------------------------------------------");
 	}
 }
